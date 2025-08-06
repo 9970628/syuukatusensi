3ada866aa90406b43454abecd597638edaee26db
sennsi <tl241203@cis.fukuoka-u.ac.jp>
2025-08-06 19:24:39 +0900

    ダンジョンまでの画面確認お願いします

--- lib/components/sennsi.dart
+++ lib/components/sennsi.dart
@@ -1,25 +1,40 @@
-import 'package:flame/components.dart'; //アニメーション切り替え
-import 'package:flame/game.dart'; //ゲーム全体
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'flash_effect.dart';
 
 class Sennsi extends SpriteAnimationComponent with HasGameRef<FlameGame> {
-  Sennsi() : super(size: Vector2.all(64)); // キャラのサイズ
+  bool moving = true; // 移動中かどうか
+  bool flashTriggered = false; // フラッシュしたかどうか
+
+  Sennsi() : super(size: Vector2.all(128));
 
   @override
   Future<void> onLoad() async {
     final image1 = await gameRef.images.load('sennsi_walk1.png');
     final image2 = await gameRef.images.load('sennsi_walk2.png');
 
-    animation = SpriteAnimation.spriteList(
-      [Sprite(image1), Sprite(image2)],
-      stepTime: 0.2, // 0.2秒ごとに切り替え
-    );
+    animation = SpriteAnimation.spriteList([
+      Sprite(image1),
+      Sprite(image2),
+    ], stepTime: 0.2);
 
-    position = Vector2(160, 400); // 初期位置
+    position = Vector2(450, 700);
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    position.y -= 50 * dt; // 上方向に移動
+
+    if (moving && position.y > 90) {
+      position.y -= 50 * dt;
+    } else if (moving) {
+      // ここで止まったことを検知
+      moving = false;
+
+      if (!flashTriggered) {
+        flashTriggered = true;
+        gameRef.add(FlashEffect()); // フラッシュを発動
+      }
+    }
   }
 }
