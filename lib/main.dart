355b036b7bd8fb4285b6da4e1018c00568d2d43a
oto1720 <kotaro17206@icloud.com>
2025-08-06 18:07:50 +0900

    mainが変わってたので修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:sennsi_app/config/router.dart';
 import 'package:sennsi_app/screens/login_screen.dart';
 // task_list_screen.dart ファイルをインポートしてGoalListScreenを使えるようにする
 import 'package:sennsi_app/screens/task_list_screen.dart';
 import 'package:sennsi_app/screens/calender_screen.dart'; //機能　開発者が作った
-
+import 'package:sennsi_app/widgets/shell.dart';
+import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'package:sennsi_app/models/task.dart';
-import 'package:sennsi_app/screens/home_screen.dart'; // 起動画面はHomeScreen
+import 'package:sennsi_app/screens/home_screen.dart';
+import 'package:device_preview/device_preview.dart' ;// 起動画面はHomeScreen
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('ja_JP', null);
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => GoalModel(),
-      child: const MyApp(),
+    DevicePreview(
+      enabled: true,
+      builder: (context) => ChangeNotifierProvider(
+        create: (context) => GoalModel(),
+        child: const MyApp(),
+      ),
     ),
   );
 }
@@ -26,15 +32,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      builder: DevicePreview.appBuilder,
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
       title: 'Goal Tracker',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         useMaterial3: true,
       ),
-      // ★★★ 最初に表示する画面をHomeScreenに設定 ★★★
-      home: const HomeScreen(),
     );
   }
 }
